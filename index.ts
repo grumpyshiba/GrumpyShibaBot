57b52ed8ab800966707b2b1fa0fa8c5352d9f266
George Ivanov <george.ivanov@ecentria.com>
2022-01-05 13:27:01 -0600

    Last revision

--- index.ts
+++ index.ts
@@ -5,12 +5,9 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN as string, {
   polling: true,
 });
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, match?.[1] as string);
-});
+// Handlers
 
-bot.onText(/\/price/, (msg) => {
+const handlePrice = (msg: TelegramBot.Message) => {
   fetch('https://api.dex.guru/v2/tokens/', { method: 'post', body: '{"ids":["0xAe448cB5A3ec77BA4aDcc6C8f9621e5921DCd77a-bsc"]}' })
   .then(respose => respose.json())
   .then(result => {
@@ -21,6 +18,41 @@ bot.onText(/\/price/, (msg) => {
     }
     bot.sendMessage(msg.chat.id, data.join('\n'), { parse_mode : "HTML" });
   });
+}
+
+// All Messages
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, match?.[1] as string);
+});
+
+bot.onText(/\/price/, handlePrice);
+
+// Private Messages
+
+bot.onText(/\/start/, (msg) => {
+  if (msg.chat.type !== 'private') { return; }
+  bot.sendMessage(msg.chat.id, 'Hi there! Select any option from list', {
+    reply_to_message_id: msg.message_id,
+    reply_markup: {
+      keyboard: 
+      [
+        [{text: '📈 Get Token Price'}],
+        [{text: '🎁 Join Airdrop'}],
+      ],
+    }
+  });
+});
+
+bot.onText(/📈 Get Token Price/, async (msg) => {
+  if (msg.chat.type !== 'private') { return; }
+  handlePrice(msg);
+});
+
+bot.onText(/🎁 Join Airdrop/, async (msg) => {
+  if (msg.chat.type !== 'private') { return; }
+  bot.sendMessage(msg.chat.id, 'Sorry, but currently there are no active airdrops. Stay tuned for updates!');
 });
 
 bot.on('message', (msg) => {
