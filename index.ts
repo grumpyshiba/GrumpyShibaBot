e9f966fa332050ef50220b70a3e665c67780fcb7
Georgii Ivanov <xercool90@gmail.com>
2022-01-28 21:49:53 -0600

    Implementation of charity function

--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
+import fs from 'fs';
 import express from 'express';
 import FormData from 'form-data';
 import fetch from 'isomorphic-fetch';
 import TelegramBot from 'node-telegram-bot-api';
 
+const tagline = fs.readFileSync('./tagline.txt', 'utf8').split('\n');
+
 const TOTAL_SUPPLY = Math.pow(10, 12);
+const CHARITY_WALLET_ADDRESS = '0x45a164217DA69B98f9cCa828cF0f22dDe0E95582';
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN as string, {
   polling: true,
 });
+
+const privateOnly = (fn: any) => (msg: TelegramBot.Message, ...args: any) => {
+  if (msg.chat.type !== 'private') { return; }
+  return fn(msg, ...args);
+};
 
 const getTokenInfo = () => {
   return fetch('https://api.dex.guru/v2/tokens/', { method: 'post', body: '{"ids":["0xAe448cB5A3ec77BA4aDcc6C8f9621e5921DCd77a-bsc"]}' })
@@ -21,7 +30,58 @@ const getTokenData = () => {
     .then((result) => result.json());
 };
 
+const getTop10 = () => {
+  return fetch('https://graphql.bitquery.io/', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-KEY': 'BQYuFrEvrfKvBjd35LxeKGREifNFY6q2'
+    },
+    body: JSON.stringify({ query: `
+      {
+        ethereum(network: bsc) {
+          transfers(
+            options: {desc: "sum_in", limit: 10}, amount: {gt: 0}, currency: { is: "0xAe448cB5A3ec77BA4aDcc6C8f9621e5921DCd77a" }, receiver: {is: "${CHARITY_WALLET_ADDRESS}"}
+          ) {
+            sum_in: amount(calculate: sum)
+            address: sender {
+              address
+              annotation
+            }
+            external
+          }
+        }
+      }
+    `})
+  }).then((response) => response.json());
+};
+
 // Handlers
+
+const handleCharity = async (msg: TelegramBot.Message) => {
+  const data = [
+    `Charity Wallet: <b>${CHARITY_WALLET_ADDRESS}</b>`,
+    ''
+  ];
+
+  try {
+    const top10 = await getTop10();
+    data.push('<b>☮️ Top 10 Funders</b>\n');
+    top10.data.ethereum.transfers.map((tx: any, index: number) => {
+      data.push(`<b>#${index + 1}</b>. ${tx.sum_in} GRUMPYSHIB (<i>${tx.address.address}</i>)`);
+    });
+  } catch(e) {
+    console.error(e);
+  }
+
+  bot.sendMessage(msg.chat.id, data.join('\n'), {
+    reply_markup: {
+      inline_keyboard: [
+        [ { text: 'Donate GrumpyShiba', url: 'https://shibhope.com/donate' } ]]
+    },
+    parse_mode: 'HTML'
+  });
+};
 
 const handlePrice = async (msg: TelegramBot.Message) => {
   const result = await getTokenInfo();
@@ -52,38 +112,32 @@ const handlePrice = async (msg: TelegramBot.Message) => {
 
 // All Messages
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, match?.[1] as string);
+bot.onText(/\/grumpy/, (msg) => {
+  bot.sendMessage(msg.chat.id, tagline[Math.floor(Math.random() * tagline.length)]);
 });
-
-bot.onText(/\/price/, handlePrice);
+bot.onText(/\/token/, handlePrice);
+bot.onText(/\/charity/, handleCharity);
 
 // Private Messages
 
-bot.onText(/\/start/, (msg) => {
-  if (msg.chat.type !== 'private') { return; }
+bot.onText(/\/start/, privateOnly((msg: TelegramBot.Message) => {
   bot.sendMessage(msg.chat.id, 'Hi there! Select any option from list', {
     reply_to_message_id: msg.message_id,
     reply_markup: {
       keyboard: 
       [
         [{text: '📈 Get Token Price'}],
+        [{text: '☮️ Charity Wallet'}],
         [{text: '🎁 Join Airdrop'}],
       ],
     }
   });
-});
-
-bot.onText(/📈 Get Token Price/, async (msg) => {
-  if (msg.chat.type !== 'private') { return; }
-  handlePrice(msg);
-});
-
-bot.onText(/🎁 Join Airdrop/, async (msg) => {
-  if (msg.chat.type !== 'private') { return; }
+}));
+bot.onText(/📈 Get Token Price/, privateOnly(handlePrice));
+bot.onText(/☮️ Charity Wallet/, privateOnly(handleCharity));
+bot.onText(/🎁 Join Airdrop/, privateOnly(async (msg: TelegramBot.Message) => {
   bot.sendMessage(msg.chat.id, 'Sorry, but currently there are no active airdrops. Stay tuned for updates!');
-});
+}));
 
 bot.on('message', (msg) => {
   console.log(msg.chat.id);
